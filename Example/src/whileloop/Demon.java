0c571d9cd059fd4750d28ba097dd335ef6e014ec
Dell <Dell@Dell>
2020-09-26 23:16:50 +0530

    Demon

---
+++ Example/src/whileloop/Demon.java
@@ -0,0 +1,14 @@
+package whileloop;
+
+public class Demon {
+
+	public Demon() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
